883fb8808bbb468d7493153e3125a5bcf67b767e
namirashi <69178123+namirashi@users.noreply.github.com>
2020-08-11 12:16:42 +0700

    Add files via upload

---
+++ Kotlin Fundamental/src/Boolean.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val nilaiA = 90
+    val nilaiB = 80
+    val nilaiC = 70
+    val nilaiD = 60
+    val nilaiE = 50
+    val now  = 75
+
+    val iskkm = now >= nilaiA && now <= nilaiB
+    print("Selamat ya!: $iskkm")
+}
